592f18c1a6b483ad4598a129053ae6ac05a9bdcf
Allan Prost <prost.allan89@gmail.com>
2017-12-20 11:00:02 +0100

    petite modifs bouton caca

--- public/js/components/Nouveautes/NouveautesSingle.js
+++ public/js/components/Nouveautes/NouveautesSingle.js
@@ -1,59 +1,53 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
 class NouveautesSingle extends React.Component {
+  constructor() {
+    super();
+    this.onClickMovetoSingle = this.onClickMovetoSingle.bind(this);
+    this.AddFav = this.AddFav.bind(this);
+  }
 
-    constructor() {
-        super();
-        this.onClickMovetoSingle = this.onClickMovetoSingle.bind(this);
-        this.AddFav = this.AddFav.bind(this);
+  onClickMovetoSingle() {
+    // envoie du state à fonction onSearchClick du container redux
+    this.props.onClickMovetoSingle(this.props.idserie);
+  }
 
-    }
-
-    onClickMovetoSingle() {
-      // envoie du state à fonction onSearchClick du container redux
-      this.props.onClickMovetoSingle(this.props.idserie);
-    }
-
-    AddFav(){
-      this.props.addFav(this.props.idserie);
-      //console.log(this.props.idserie);
-    }
-
-
+  AddFav() {
+    this.props.addFav(this.props.idserie);
+    //console.log(this.props.idserie);
+  }
+ 
   render() {
-
     return (
-        <li 
-          className="col-xs-6 col-md-4 project"
-          data-groups="[&quot;illustration&quot;]"
-        >
-          <div className="img-bg-color primary">
-            <Link to={this.props.link} className="project-link"></Link>
+      <li
+        className="col-xs-6 col-md-4 project"
+        data-groups="[&quot;illustration&quot;]"
+      >
+        <div className="img-bg-color primary">
+          <Link to={this.props.link} className="project-link" />
 
-            <img src={this.props.img} alt="" />
+          <img src={this.props.img} alt="" />
 
-            <div className="project-hover-tools" >
-              <Link to={this.props.link} className="view-btn">
-                <i className="lnr lnr-eye" onClick={this.onClickMovetoSingle} />
-              </Link>
-                <i className="lnr lnr-poop view-btn" onClick={this.AddFav}/>
-            </div>
-            
-            
-
-            <div className="project-details">
-              <h5 className="project-title">{this.props.title}</h5>
-              <p className="skill">{this.props.description}</p>
-            </div>
+          <div className="project-hover-tools">
+            <Link to={this.props.link} className="view-btn">
+              <i className="lnr lnr-eye" onClick={this.onClickMovetoSingle} />
+            </Link>
+            <Link to="/home" className="view-btn">
+              <i className="lnr lnr-poop" onClick={this.AddFav} />
+            </Link>
           </div>
-        </li>
+
+          <div className="project-details">
+            <h5 className="project-title">{this.props.title}</h5>
+            <p className="skill">{this.props.description}</p>
+          </div>
+        </div>
+      </li>
     );
   }
 }
-
-
 
 function mapDispatchToProps(dispatch, props) {
   return {
@@ -63,7 +57,6 @@ function mapDispatchToProps(dispatch, props) {
     addFav: function(value) {
       dispatch({ type: "addToNuts", nutSerie: value });
     }
-    
   };
 }
 
