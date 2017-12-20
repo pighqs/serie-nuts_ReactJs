5909d45322d5e2eb142c289f3d2f9c914cb68ac9
pighqs <peggy.hqs@gmail.com>
2017-12-20 14:43:30 +0100

    add save db id

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express   = require('express');
 var app = express();
 
+
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -14,6 +15,29 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(fileUpload());
 
+//// CONNECTION DB MLAB
+var mongoose = require("mongoose");
+var options = { server: { socketOptions: { connectTimeoutMS: 30000 } },  };
+
+mongoose.connect(
+  "mongodb://cowboys:serienuts@ds121534.mlab.com:21534/serienuts",
+  {useMongoClient: true},
+  function(err) {
+    if (err) {
+      console.log("erreur : " + err);
+    } else {
+      console.log("bien connecté à DB Mlab serie_nuts");
+    }
+  }
+);
+
+// schemas
+var nutSchema = mongoose.Schema({
+  nutFromDB_id: Number, 
+});
+
+// models
+var NutModel = mongoose.model("nuts", nutSchema);
 var nuts = [];
 
 app.get('/', function(req, res) {
@@ -21,9 +45,23 @@ app.get('/', function(req, res) {
 });
 
 app.post('/addfav', function(req, res) {
-   nuts.push(req.body.nut_id);
-  console.log(nuts);
-  res.json(nuts);
+  var newNut = new NutModel({
+    nutFromDB_id: req.body.nut_id,
+  });
+
+  newNut.save(function(error, nut) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("save DB ok" + nut);
+      res.json(nut);
+    }
+
+  });
+// nuts.push(req.body.nut_id);
+//  console.log(nuts);
+ //res.json(nuts);
+
 });
 
 
