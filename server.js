d6878724464b7d303d923d9c6a38e98bdb1ba426
pighqs <peggy.hqs@gmail.com>
2017-12-20 17:28:15 +0100

    add fav in db and icon change

--- server.js
+++ server.js
@@ -38,13 +38,27 @@ var nutSchema = mongoose.Schema({
 
 // models
 var NutModel = mongoose.model("nuts", nutSchema);
-var nuts = [];
+
 
 app.get('/', function(req, res) {
   res.render('index');
 });
 
+app.get("/findnuts", function(req, res) {
+  var nutIDsFromDB = [];
+  NutModel.find(function(err, nuts) {
+    //console.log('dans find');
+    var nutFromDB;
+    for (var i = 0; i < nuts.length; i++) {
+      nutIDfromDB = nuts[i].nutFromDB_id;
+      nutIDsFromDB.push(nutIDfromDB);
+    }
+    res.json(nutIDsFromDB);
+  })
+});
+
 app.post('/addfav', function(req, res) {
+ 
   var newNut = new NutModel({
     nutFromDB_id: req.body.nut_id,
   });
@@ -53,14 +67,17 @@ app.post('/addfav', function(req, res) {
     if (error) {
       console.log(error);
     } else {
-      console.log("save DB ok" + nut);
+      console.log("save DB ok : " + nut);
       res.json(nut);
     }
 
   });
-// nuts.push(req.body.nut_id);
-//  console.log(nuts);
- //res.json(nuts);
+
+
+});
+
+app.post('/delfav', function(req, res) {
+  console.log('id to delete : ' + req.body.nut_id)
 
 });
 
