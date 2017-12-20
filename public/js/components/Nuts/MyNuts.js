a7698a38d84994867410d37ed06d019bc970b9b0
Allan Prost <prost.allan89@gmail.com>
2017-12-20 10:23:47 +0100

    correction mynuts

--- public/js/components/Nuts/MyNuts.js
+++ public/js/components/Nuts/MyNuts.js
@@ -4,101 +4,54 @@ import MyNutsSingle from "./MyNutsSingle";
 
 import { connect } from "react-redux";
 
-
 class MyNuts extends React.Component {
   constructor() {
     super();
     this.state = {
-      returnSeriesFromAPI: ""
+      favoriteShowsData: []
     };
-}
+  }
 
   componentDidMount() {
-    var that = this;
-    var nutsToFetch = this.props.nuts.join();
-    var requete =
-      "https://api.betaseries.com/shows/display?key=d0c44a7cd167&id=" +
-      nutsToFetch;
-    console.log(requete);
-    fetch(requete)
-      .then(response => response.json())
-      .then(function(datas) {
-        if(that.props.nuts.length > 1) {
-            that.setState({
-              returnSeriesFromAPI: datas.shows
-            });
-        } else {
-            that.setState({
-                returnSeriesFromAPI: datas.show
-              });
-        }
-      })
-      .catch(error => console.log("erreur fetch MyNuts !!!" + error));
-    
+    if (this.props.nuts.length) {
+      var that = this;
+      var nutsToFetch = this.props.nuts.join();
+      var requete =
+        "https://api.betaseries.com/shows/display?key=d0c44a7cd167&id=" +
+        nutsToFetch;
+      fetch(requete)
+        .then(response => response.json())
+        .then(function(data) {
+          that.setState({
+            favoriteShowsData: data.show ? [data.show] : data.shows
+          });
+        })
+        .catch(error => console.log("erreur fetch MyNuts !!!" + error));
+    }
   }
 
   render() {
-    var NutList = [];
-    console.log(Array.isArray(this.state.returnSeriesFromAPI));
-    if (this.state.returnSeriesFromAPI === undefined) {
-        NutList = <div className="alert alert-nuts alert-dismissible">You Have no Nuts!</div>;
-    } else {
-        if(Array.isArray(this.state.returnSeriesFromAPI) === true) {
-            var lengthStateDatas;
-            this.state.returnSeriesFromAPI.length > 9
-              ? (lengthStateDatas = 9)
-              : (lengthStateDatas = this.state.returnSeriesFromAPI.length);
-            for (var i = 0; i < lengthStateDatas; i++) {
-              var poster;
-              if (this.state.returnSeriesFromAPI[i].images.poster !== undefined) {
-                poster = this.state.returnSeriesFromAPI[i].images.poster;
-              } else {
-                poster = "./images/default-poster.jpg";
-              }
-        
-              NutList.push(
-                <MyNutsSingle
-                  title={this.state.returnSeriesFromAPI[i].title}
-                  description={this.state.returnSeriesFromAPI[i].description}
-                  img={poster}
-                  link="/affichageseriesingle"
-                  idserie={this.state.returnSeriesFromAPI[i].id}
-                  key={i}
-                />
-              );
-            }
-        } if (Array.isArray(this.state.returnSeriesFromAPI) === false){
-              var poster;
-              if (this.state.returnSeriesFromAPI.images.poster !== undefined) {
-                poster = this.state.returnSeriesFromAPI.images.poster;
-              } else {
-                poster = "./images/default-poster.jpg";
-              }
-        
-              NutList.push(
-                <MyNutsSingle
-                  title={this.state.returnSeriesFromAPI.title}
-                  description={this.state.returnSeriesFromAPI.description}
-                  img={poster}
-                  link="/affichageseriesingle"
-                  idserie={this.state.returnSeriesFromAPI.id}
-                  key={i}
-                />
-              );
-            
-        }
-
-
+    if (!this.state.favoriteShowsData || !this.state.favoriteShowsData.length) {
+      <div className="alert alert-nuts alert-dismissible">
+        You Have no Nuts!
+      </div>;
     }
-
-
     return (
-        
       <section id="portfolio">
         <div className="container">
           <ul className="row portfolio list-unstyled lightbox" id="grid">
-            {NutList}
-
+            {this.state.favoriteShowsData.map((favoriteShow, index) => (
+              <MyNutsSingle
+                title={favoriteShow.title}
+                description={favoriteShow.description}
+                img={
+                  favoriteShow.images.poster || "./images/default-poster.jpg"
+                }
+                link="/affichageseriesingle"
+                idserie={favoriteShow.id}
+                key={index}
+              />
+            ))}
             <li className="col-xs-6 col-md-4 shuffle_sizer" />
           </ul>
         </div>
@@ -108,7 +61,7 @@ class MyNuts extends React.Component {
 }
 
 function mapStateToProps(state) {
-    return { nuts: state.nutSerie };
-  }
-  var MyNutsRedux = connect(mapStateToProps, null)(MyNuts);
+  return { nuts: state.nutSerie };
+}
+var MyNutsRedux = connect(mapStateToProps, null)(MyNuts);
 export default MyNutsRedux;
