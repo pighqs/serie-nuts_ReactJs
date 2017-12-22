6f0752467122c54b687b6a0fe8590898dc188d29
pighqs <peggy.hqs@gmail.com>
2017-12-22 16:20:56 +0100

    add favicon and jackson 5

--- public/js/components/About/Team.js
+++ public/js/components/About/Team.js
@@ -10,7 +10,7 @@ class Team extends React.Component {
       <section id="team">
         <h2 className="text-center space-top-2x">THE TEAM</h2>
         <p className="text-center space-bottom-2x">
-          The amazing team of hard-working designers and developers.
+          The amazing team of hard-working developers.
         </p>
         <div className="row">
           <div className="col-sm-6">
@@ -33,10 +33,10 @@ class Team extends React.Component {
               <img src="images/team_AF.jpg" alt="" />
               <div className="team-info-box">
                 <h6>PEGGY HUYNH-QUAN-SUU</h6>
-                <p className="team-role">Graphic Designer</p>
+                <p className="team-role">FullStack Developper</p>
                 <p className="social text-center">
-                  <a target="_blank" href="https://www.linkdein.com"><i className="fa fa-linkedin"></i></a>
-                  <a target="_blank" href="https://www.twitter.com"><i className="fa fa-twitter"></i></a>
+                  <a target="_blank" href="https://www.linkedin.com/in/peggyhqs/"><i className="fa fa-linkedin"></i></a>
+                  <a target="_blank" href="https://youtu.be/9_G637sWOJk"><i className="fa fa-twitter"></i></a>
                   <a target="_blank" href="https://www.instagram.com"><i className="fa fa-instagram"></i></a>
                 </p>
               </div>
